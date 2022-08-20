291585788e545cc9bc409ffb03918f096e85b0c4
Jordan <Jordan@JarvisPC.bbrouter>
2022-08-20 19:29:07 +0530

    Commit 2

---
+++ 27NovEvening/src/WebTable/A_CountRowSize.java
@@ -0,0 +1,15 @@
+package WebTable;
+
+
+public class A_CountRowSize {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","H:\\Selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		
+	
+	
+	
+	}
+
+}
